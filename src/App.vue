dc7daae59843bce69f066fe3c207c6cb490bf873
Arpad Nyircsak <69168570+Pyriel77@users.noreply.github.com>
2023-10-02 15:17:18 +0200

    scss rev2.0 + date formatted

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,11 @@
             class="event-name"
             :style="{ backgroundColor: currentEvent.color }"
           >
-            {{ currentEvent.name }}
+            {{ currentEvent.name.toUpperCase() }}
           </div>
           <div class="event-date">
-            {{ currentEvent.from }} - {{ currentEvent.to }}
+            {{ formatDate(currentEvent.from) }} -
+            {{ formatDate(currentEvent.to) }}
           </div>
           <div class="talent-container">
             <div
@@ -37,6 +38,7 @@ export default {
     return {
       overlayVisible: false,
       currentEvent: {},
+      formattedDate: '',
       eventData: [
         {
           name: 'FGC',
@@ -64,7 +66,6 @@ export default {
   },
   methods: {
     toggleOverlay() {
-      console.log(this.overlayVisible);
       this.overlayVisible = !this.overlayVisible;
       if (this.overlayVisible) {
         const randomIndex = Math.floor(Math.random() * this.eventData.length);
@@ -72,6 +73,29 @@ export default {
       } else {
         this.currentEvent = {};
       }
+    },
+    formatDate(inputDate) {
+      const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ];
+
+      const dateParts = inputDate.split('-');
+
+      const monthIndex = parseInt(dateParts[1]) - 1;
+      const day = dateParts[2];
+
+      return `${months[monthIndex]} ${parseInt(day)}`;
     },
   },
 };
@@ -94,7 +118,6 @@ export default {
 }
 
 .overlay {
-  /*display: none;*/
   position: fixed;
   top: 0;
   left: 0;
@@ -123,52 +146,73 @@ canvas {
 }
 
 .event-details {
+  position: relative;
   background-color: #252525;
-  border: 2px solid #fff;
-  border-radius: 10px;
+  border: 4px solid #fff;
+  border-radius: 20px;
   text-align: center;
-  padding: 3px;
+  vertical-align: bottom;
+  height: 32%;
+  width: 50%;
 }
 
 .event-name {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   color: #fff;
-  border-top-right-radius: 5px;
-  border-top-left-radius: 5px;
-  font-size: 36px;
-  font-weight: 200;
-  text-shadow: 2px 2px #252525;
-  margin-bottom: 10px;
+  border-top-right-radius: 10px;
+  border-top-left-radius: 10px;
+  font-size: 42px;
+  font-weight: 600;
+  text-shadow: 3px 3px #252525;
+
+  height: 7rem;
+  margin: 7px;
 }
 
 .event-date {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: #fff;
   color: #000;
   font-size: 24px;
-  margin-bottom: 10px;
+  margin-bottom: 7px;
+  height: 3rem;
+  margin: 7px;
 }
 
 .talent-container {
   display: flex;
   flex-direction: row;
   align-items: center;
+  width: auto;
+  height: 7rem;
+  margin: 7px 5px;
 }
 
 .event-talent {
-  color: #fff;
-  font-size: 20px;
-  margin: 2px;
   display: flex;
-  gap: 10px;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  font-size: 36px;
+  margin: 2px;
   text-shadow: 2px 2px #252525;
+  width: 100%;
+  height: 100%;
+  text-align: center;
 }
 
 .talent-container > div:first-child {
   text-align: left;
-  border-bottom-left-radius: 5px;
+  border-bottom-left-radius: 10px;
 }
 
 .talent-container > div:last-child {
   text-align: right;
-  border-bottom-right-radius: 5px;
+  justify-content: center;
+  border-bottom-right-radius: 10px;
 }
 </style>
